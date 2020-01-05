78af37d2e0fc16fb3e64b4ffde359b3265259d9a
aboodsy963 <51748117+aboodsy963@users.noreply.github.com>
2020-01-05 22:49:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,53 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
-Client.on("guildMemberAdd", member => {
+console.log("Welcome Again !");
+ 
+ client.on("guildMemberRemove", member => {
   member.createDM().then(function (channel) {
-	  const Embed11 = new Discord.RichEmbed()
-        .setColor("RANDOM")
-  .setDescription(`حياك يقلبي ♥
-  https://discord.gg/SY`) 
-return channel.sendEmbed(Embed11)
+  return channel.send(` 
+**
+:kissing::heart::eyes: حيااكْ الله  فى سيرفرنا 
+╔╦╦╦═╦╗╔═╦═╦══╦═╗
+║║║║╩╣╚╣═╣║║║║║╩╣
+╚══╩═╩═╩═╩═╩╩╩╩═╝ 
 
+‏نقدم لك دعوة لسموك الراقيَ .
+‏لنحظى بجمال حضورك وتواجدك
+•بتنـورونـا الجميع :wink:
+•اي مشكله توآجهك ولا يهمك. عندك الاداره تفضل ونحل مشكلتك مع آي شخص بالسيرفر.:telephone_receiver:
+•عندك فكره موضوع تفضل شآركنا فيه:jack_o_lantern::gift:
+•واخيرا حييييياكم واكيد وجودكم هو اللي بيدعمنا وراح نكون الافضل.
+شرفنا يا عسل:wink: :kissing_heart:
+
+https://discord.gg/SY                            
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`)
+}).catch(console.error)
+
+})
+ 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+:kissing::heart::eyes: حيااكْ الله  فى سيرفرنا 
+╔╦╦╦═╦╗╔═╦═╦══╦═╗
+║║║║╩╣╚╣═╣║║║║║╩╣
+╚══╩═╩═╩═╩═╩╩╩╩═╝ 
+
+‏نقدم لك دعوة لسموك الراقيَ .
+‏لنحظى بجمال حضورك وتواجدك
+•بتنـورونـا الجميع :wink:
+•اي مشكله توآجهك ولا يهمك. عندك الاداره تفضل ونحل مشكلتك مع آي شخص بالسيرفر.:telephone_receiver:
+•عندك فكره موضوع تفضل شآركنا فيه:jack_o_lantern::gift:
+•واخيرا حييييياكم واكيد وجودكم هو اللي بيدعمنا وراح نكون الافضل.
+شرفنا يا عسل:wink: :kissing_heart:
+
+
+https://discord.gg/SY                            
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`) 
 }).catch(console.error)
 })
-
-
-
-
-
-
-client.login("NjE1NjU3NTI4OTQ4MTYyNTgw.XhI0XA.2CJCW71wl5zsQPzLDIOuwOWrKAE");
+client.login('NjE1NjU3NTI4OTQ4MTYyNTgw.XhI1_w.CJqS1Tln5zttBu_CVZADBgFzp2M');
